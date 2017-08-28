cd1a3a659fca24bfb3aaa088f9b9781edbce5fd9
Hodogor <kop.lop2012@yandex.ru>
2017-08-28 15:52:56 +0300

    Обновление снилса
    Удалил скрипт MSSQL,
    Добавил запрос для обновления СНИЛСа,
    Добавил проверку документов

--- PyRlouderSend.py
+++ PyRlouderSend.py
@@ -30,96 +30,19 @@ for row in rows:
     # Узнаем паспортные данные пациента из профиля
     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=10.14.198.200;DATABASE=AKUZDB;UID=sa;PWD=Q!w2E#r4')
     cursor = cnxn.cursor()
-    sql = "	SELECT [DocSeries],[DocNumber], [SNILS], [DocType] FROM [AKUZDB].[dbo].[T_PATIENT] WHERE [PatientID]='"+PatientID +"'"
+    sql = "SELECT [DocSeries],[DocNumber], [SNILS], [DocType] FROM [AKUZDB].[dbo].[T_PATIENT] WHERE [PatientID]='"+PatientID +"'"
     cursor.execute(sql)
     patientDoc = cursor.fetchone()
+    cnxn.commit()
     #Объявляем переменные
     DocSeries, DocNumber, SNILS, DocType = patientDoc  
-    print(SNILS + '=' +row[2])
-"""cursor = cnxn.cursor()
-ex = 
-/****** Проверка на наличие документов ******/
-
---объявляем переменные
-declare @DirectionID varchar(250)
-	   ,@Patient varchar(250)
-	   ,@DocType varchar(250)
-	   ,@DocSeries varchar(16)
-	   ,@DocNumber varchar(16)
-	   ,@NewDocNumber varchar(16)
-	   ,@SNILS char(14)
-	   ,@double int
-
---объявляем курсор
-DECLARE my_cur CURSOR 
-FOR 
-SELECT [PatientBenefitsDirectionID]
-	,[Patient]
-	,[SNILS]
-	,[DocType]
-	,[DocSeries]
-	,[DocNumber] FROM  [AKUZDB].[dbo].[T_PATIENT_BENEFITS_DIRECTION] WHERE [Loaded]=0
-
---открываем курсор
-OPEN my_cur
-
---считываем данные первой строки в наши переменные
-FETCH NEXT FROM my_cur INTO @DirectionID, @Patient, @SNILS,@DocType,@DocSeries,@DocNumber
-
---если данные в курсоре есть, то заходим в цикл
---и крутимся там до тех пор, пока не закончатся строки в курсоре
-WHILE @@FETCH_STATUS = 0
-BEGIN
-	--на каждую итерацию цикла запускаем нашу основную процедуру с нужными параметрами   
-	SELECT @DocSeries=[DocSeries] 
-      , @DocNumber=[DocNumber]
-      , @SNILS=[SNILS]
-      , @DocType=[DocType]
-       FROM [AKUZDB].[dbo].[T_PATIENT]
-	WHERE [PatientID]= @Patient
-	
-	
-	
-	-- удаляем направления без документов
-	if @DocNumber is NULL OR @DocSeries is NULL
-		Begin
-			PRINT 'Нет документов';
-			-- удаляем льготу
-			Delete 
-			From [T_PATIENT_BENEFITS_DIRECTION_DATA]
-			where
-				T_PATIENT_BENEFITS_DIRECTION_DATA.PatientBenefitsDirection = @DirectionID;
-			-- удаляем направление
-			Delete 
-			From [T_PATIENT_BENEFITS_DIRECTION]
-			where
-				T_PATIENT_BENEFITS_DIRECTION.PatientBenefitsDirectionID = @DirectionID;
-		End
-		
-	-- удаляем направления если есть дубль 
-	SELECT @double = Count([T_PATIENT_BENEFITS_DIRECTION].[Patient]) FROM  [AKUZDB].[dbo].[T_PATIENT_BENEFITS_DIRECTION] WHERE [Loaded]=0 AND [Patient]=@Patient
-	PRINT @double;
-	if @double <> 1
-		Begin
-			PRINT 'дубль';
-			-- удаляем льготу
-			Delete 
-			From [T_PATIENT_BENEFITS_DIRECTION_DATA]
-			where
-				T_PATIENT_BENEFITS_DIRECTION_DATA.PatientBenefitsDirection = @DirectionID;
-			-- удаляем направление
-			Delete 
-			From [T_PATIENT_BENEFITS_DIRECTION]
-			where
-				T_PATIENT_BENEFITS_DIRECTION.PatientBenefitsDirectionID = @DirectionID;
-		End
-	
-	--считываем следующую строку курсора
-	FETCH NEXT FROM my_cur INTO @DirectionID,@Patient, @SNILS,@DocType,@DocSeries,@DocNumber
-END
---закрываем курсор
-CLOSE my_cur
-DEALLOCATE my_cur
-    
-   
-cursor.execute(ex)"""
+    # Обновляем СНИЛС
+    if SNILS != row[2]:
+        cursor = cnxn.cursor()
+        cursor.execute("UPDATE [T_PATIENT_BENEFITS_DIRECTION] SET [SNILS] = ? WHERE [PatientBenefitsDirectionID] = ?;", SNILS , row[0])
+        cnxn.commit()
+        print("Обновляю данные:"+SNILS + '=' +row[2])
+        
+    # Обновляем свидетельство
+    if DocSeries != row[4] or DocNumber != row[5]:
+        print("Обновляю данные:"+DocSeries + '=' +row[4]+' '+row[5])
